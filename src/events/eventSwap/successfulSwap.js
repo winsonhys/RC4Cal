4a29be7c47da7a358f37907c0a87074e3fb76cb9
Winson Heng Yong Seng <e0148571@soctf-imac-003.comp.nus.edu.sg>
2018-07-31 00:16:33 +0800

    url fix

--- src/events/eventSwap/successfulSwap.js
+++ src/events/eventSwap/successfulSwap.js
@@ -7,12 +7,12 @@ export default class SuccessfulSwap extends React.Component {
   obtainEventIdFromAndToObject = () => {
     const eventIds = URL.parse(window.location.href).query
     const splitQueries = eventIds.split("&")
-    const eventIdFromArray = splitQueries[0].split("=")
-    const eventIdToArray = splitQueries[1].split("=")
-
+    const eventIdFromArray = splitQueries[0].split("eventIdFrom=")
+    const eventIdToArray = splitQueries[1].split("eventIdTo=")
+    console.log(eventIdToArray)
     return {
-      [eventIdFromArray[0]]: eventIdFromArray[1],
-      [eventIdToArray[0]]: eventIdToArray[1],
+      eventIdFrom: eventIdFromArray[1],
+      eventIdTo: eventIdToArray[1],
     }
   }
   componentDidMount = async () => {
