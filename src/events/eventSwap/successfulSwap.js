70cda3e22e6b11cded92285380ace314402373eb
Winson Heng Yong Seng <e0148571@soctf-imac-003.comp.nus.edu.sg>
2018-07-30 23:53:10 +0800

    events actually swap now

--- src/events/eventSwap/successfulSwap.js
+++ src/events/eventSwap/successfulSwap.js
@@ -1,7 +1,24 @@
 import React from "react"
 import PropTypes from "prop-types"
+import URL from "url"
+import { eventSwap } from "../../common/api/events"
 
 export default class SuccessfulSwap extends React.Component {
+  obtainEventIdFromAndToObject = () => {
+    const eventIds = URL.parse(window.location.href).query
+    const splitQueries = eventIds.split("&")
+    const eventIdFromArray = splitQueries[0].split("=")
+    const eventIdToArray = splitQueries[1].split("=")
+
+    return {
+      [eventIdFromArray[0]]: eventIdFromArray[1],
+      [eventIdToArray[0]]: eventIdToArray[1],
+    }
+  }
+  componentDidMount = async () => {
+    const eventIds = this.obtainEventIdFromAndToObject()
+    await eventSwap(eventIds.eventIdFrom, eventIds.eventIdTo)
+  }
   render() {
     return (
       <div>
